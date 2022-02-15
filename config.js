6d78fa208c33e18198849c8a6b25dc0835cb0ff0
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-02-15 22:45:27 +0530

    ```Ultroid V4.0.0```

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
+ 
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
