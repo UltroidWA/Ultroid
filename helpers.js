6dc969370d1afc418db7f94a269ddca659a5b883
NICO <90969977+SLTechnicalTips@users.noreply.github.com>
2022-02-15 20:55:02 +0530

    .

---
+++ helpers.js
@@ -0,0 +1,16 @@
+function successfullMessage(msg) {
+    return "✅ *Ultroid*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🛑 *Ultroid*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "⏺️ *Ultroid*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
