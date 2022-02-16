4242a547eb9bdba7b9ef8f321002b2c9ec04daee
✬  CYBER DRAXO ✬ <99461944+CyberDraxo@users.noreply.github.com>
2022-02-16 12:23:50 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ async function whatsAsena () {
     
     const conn = new WAConnection();
     const Session = new StringSession();
-    conn.version = [3, 3234, 9]
+    conn.version = [2, 2142, 12]
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
     var nodb;
